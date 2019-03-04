2659a95d5a787a25b46bcdd8b17a7abdd6734939
loganathan <loganathan@LOGANATHANV-PC>
2019-03-04 17:42:06 +0530

    Add project files.

---
+++ MyFirstGithub/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace MyFirstGithub.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ MyFirstGithub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFirstGithub.Global" Language="C#" %>
